3fa12de403905f373e77103a2e73a933c1b68de7
Syd Xu <prof.syd.xu@gmail.com>
2020-05-07 12:23:21 +0800

    add SourceDisplayName in event struct

--- model/event.go
+++ model/event.go
@@ -7,29 +7,29 @@ import (
 )
 
 type Event struct {
-	Type              EventType  `json:"type,omitempty"`
-	MsgId             uint64     `json:"msg_id,omitempty"`
-	MsgIdStr          string     `json:"msg_id_str,omitempty"`
-	MsgType           MsgType    `json:"msg_type,omitempty"`
-	FromUser          string     `json:"from_user,omitempty"`
-	FromUserNick      string     `json:"from_user_nick,omitempty"`
-	FromUserAvatar    string     `json:"from_user_head,omitempty"`
-	Sender            string     `json:"sender,omitempty"`
-	ToUser            string     `json:"to_user,omitempty"`
-	Content           string     `json:"content,omitempty"`
-	Url               string     `json:"url,omitempty"`
-	Image             string     `json:"image,omitempty"`
-	Title             string     `json:"title,omitempty"`
-	SourceDisplayName string     `json:"source_display_name,omitempty"`
-	AppOriId          string     `json:"app_ori_id,omitempty"`
-	AppId             string     `json:"appid,omitempty"`
-	Path              string     `json:"path,omitempty"`
-	Style             EventStyle `json:"style,omitempty"`
-	Contacts          []Contact  `json:"contacts,omitempty"`
-	ContactCount      uint       `json:"contact_count,omitempty"`
-	CreateTime        int64      `json:"create_time,omitempty"`
-	Group             *Group     `json:"group,omitempty"`
-	AtList            string     `json:"at_list,omitempty"`
+	Type           EventType  `json:"type,omitempty"`
+	MsgId          uint64     `json:"msg_id,omitempty"`
+	MsgIdStr       string     `json:"msg_id_str,omitempty"`
+	MsgType        MsgType    `json:"msg_type,omitempty"`
+	FromUser       string     `json:"from_user,omitempty"`
+	FromUserNick   string     `json:"from_user_nick,omitempty"`
+	FromUserAvatar string     `json:"from_user_head,omitempty"`
+	Sender         string     `json:"sender,omitempty"`
+	ToUser         string     `json:"to_user,omitempty"`
+	Content        string     `json:"content,omitempty"`
+	Url            string     `json:"url,omitempty"`
+	Image          string     `json:"image,omitempty"`
+	Title          string     `json:"title,omitempty"`
+	AppName        string     `json:"app_name,omitempty"`
+	AppOriId       string     `json:"app_ori_id,omitempty"`
+	AppId          string     `json:"appid,omitempty"`
+	Path           string     `json:"path,omitempty"`
+	Style          EventStyle `json:"style,omitempty"`
+	Contacts       []Contact  `json:"contacts,omitempty"`
+	ContactCount   uint       `json:"contact_count,omitempty"`
+	CreateTime     int64      `json:"create_time,omitempty"`
+	Group          *Group     `json:"group,omitempty"`
+	AtList         string     `json:"at_list,omitempty"`
 }
 
 type EventRequest struct {
